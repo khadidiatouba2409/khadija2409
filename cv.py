92bda4410cff3c6df5e0e5bfa9141cd7bf667fe7
khadidiatouba2409 <khadidiatouba2409@gmail.com>
2026-02-26 20:37:43 +0100

    Update cv.py

--- cv.py
+++ cv.py
@@ -1,7 +1,5 @@
 import streamlit as st
-
 with st.sidebar:
-     st.write("khad.jpeg")
     st.write("CONTACTS")
     st.write("Email: khadidiatouba2409@gmail.com")
     st.write("Tel: 78 478 7313")
@@ -10,6 +8,7 @@ with st.sidebar:
     st.write("2024-2026: BTS")
     st.write("2023-2024: BACCALEAURIAT S2")
 st.header("Technicienne supérieur en Geomatique")
+
 
 
 
