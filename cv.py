1f3d68d6264b90a09dd0e044c83ea70edc43c491
khadidiatouba2409 <khadidiatouba2409@gmail.com>
2026-02-26 19:57:51 +0100

    Update cv.py

--- cv.py
+++ cv.py
@@ -1,11 +1,12 @@
 import streamlit as st
 st.write("Curriculum Vitae")
 with st.sidebar
-st image("khad.jpeg")
+
 st write ("KHADIDIATOU BA")
 st write("CONTACTS")
 st write("Email:khadidiatouba2409@gmail.com")
 st write("Adresse: Mermoz/Sacré-Coeur1")
+
 
 
 
