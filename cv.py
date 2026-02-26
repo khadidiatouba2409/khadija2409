e69045fa4d0381355be416f90da543d67636017a
khadidiatouba2409 <khadidiatouba2409@gmail.com>
2026-02-26 23:16:53 +0100

    Update cv.py

--- cv.py
+++ cv.py
@@ -5,7 +5,7 @@ with st.sidebar:
     st.write("Tel: 78 478 7313")
     st.write("Adresse: MERMOZ/SACRE-COEUR1")
 
-    st.divise()
+    
 
     st.markdown("FORMATIONS")
     st.write("2025-2026 : BTS Géomatique")
@@ -17,6 +17,7 @@ st.header("Technicienne Supérieure en Geomatique")
 
 st.subheader("A propos de moi")
 st.write("Passionnée par la géomatique, je suis une personne sérieuse et dynamique, dotée d’un bon esprit d’analyse et d’une bonne capacité de travail en équipe.")
+
 
 
 
