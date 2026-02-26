242ce237733b7fedca0abdfd73f95bd87c1446b7
khadidiatouba2409 <khadidiatouba2409@gmail.com>
2026-02-26 20:28:48 +0100

    Update cv.py

--- cv.py
+++ cv.py
@@ -1,10 +1,19 @@
 import streamlit as st
-st.write("Curriculum Vitae")
-with st.sidebar : 
-st.write ("KHADIDIATOU BA"),
-st.write("CONTACTS"),
-st.write("Email:khadidiatouba2409@gmail.com"),
-st.write("Adresse: Mermoz/Sacré-Coeur1"),
+
+with st.sidebar:
+    st.image("DIOP.jpeg")
+    st.write("Papa Demba DIOP")
+    st.write("CONTACTS")
+    st.write("Email: dpapademba59@gmail.com")
+    st.write("Tel: 78147739")
+    st.write("Adresse: Medina,rue 06")
+    st.write("FORMATIONS")
+    st.write("2021-2022: BAC L2")
+    st.write("2022-2025: Licence en Geographie")
+    st.write("2025-2026:BTS en Geomatique")
+
+st.header("Technicien supérieur en Geomatique")
+
 
 
 
