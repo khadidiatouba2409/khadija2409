b78dbbe0fdee84d8a7518f37dd7346d63afe5309
khadidiatouba2409 <khadidiatouba2409@gmail.com>
2026-02-26 23:40:23 +0100

    Update cv.py

--- cv.py
+++ cv.py
@@ -12,12 +12,17 @@ with st.sidebar:
 - BTS en Géomatique — 2024/2026    
 - Baccalauréat S2 — 2023/2024
 """)
+    st.write("Compétences personnelles")
+    st.write("""•	Sérieuse
+             •	Motivée
+             •	disciplinée""")
    
     
 st.header("Technicienne Supérieure en Geomatique")
 
 st.subheader("A propos de moi")
 st.write("Passionnée par la géomatique, je suis une personne sérieuse et dynamique, dotée d’un bon esprit d’analyse et d’une bonne capacité de travail en équipe.")
+
 
 
 
