a61c07ce8d23d5483a6430ca03074f0ea9bc6644
khadidiatouba2409 <khadidiatouba2409@gmail.com>
2026-02-26 23:24:24 +0100

    Update cv.py

--- cv.py
+++ cv.py
@@ -1,9 +1,9 @@
 import streamlit as st
 with st.sidebar:
     st.write("CONTACTS")
-    st.write("Email: khadidiatouba2409@gmail.com")
-    st.write("Tel: 78 478 7313")
-    st.write("Adresse: MERMOZ/SACRE-COEUR1")
+    st.write("📧 Email: khadidiatouba2409@gmail.com")
+    st.write("📱Tel: 78 478 7313")
+    st.write("📍Adresse: MERMOZ/SACRE-COEUR1")
 
     
 
@@ -17,6 +17,7 @@ st.header("Technicienne Supérieure en Geomatique")
 
 st.subheader("A propos de moi")
 st.write("Passionnée par la géomatique, je suis une personne sérieuse et dynamique, dotée d’un bon esprit d’analyse et d’une bonne capacité de travail en équipe.")
+
 
 
 
