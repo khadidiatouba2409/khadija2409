4c482bb7c4ee8c1e388599497bb415322a1a66cf
khadidiatouba2409 <khadidiatouba2409@gmail.com>
2026-02-26 20:59:08 +0100

    Update cv.py

--- cv.py
+++ cv.py
@@ -4,11 +4,13 @@ with st.sidebar:
     st.write("Email: khadidiatouba2409@gmail.com")
     st.write("Tel: 78 478 7313")
     st.write("Adresse: MERMOZ/SACRE-COEUR1")
+   st.divider()  
     st.markdown("FORMATIONS")
     st.write("2025-2026 : BTS Géomatique")
     st.write("2024-2026: BTS")
     st.write("2023-2024: BACCALEAURIAT S2")
 st.header("Technicienne supérieur en Geomatique")
+
 
 
 
